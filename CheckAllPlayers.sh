2f4fa5a10aa085a037852a9d208ba7988dd068f6
WBigaj <wiktorbigaj@wp.pl>
2024-08-24 10:00:42 +0000

    Changed output file for file with date of execution

--- CheckAllPlayers.sh
+++ CheckAllPlayers.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 #S1 is decrypted file with the PlayerList
-#$2 is file for the output
 
 tmp1=$(mktemp /tmp/tmp.XXXX)
+outputFile="$(date +"%Y-%m-%d-%H-%M-%S")-Results.txt"
 
 gpg -d $1 > $tmp1
 
 while IFS= read -r line || [[ -n "$line" ]]; do
     echo "Checking $line ...";
     if [ "$(./CheckProfile.sh $line)" == "$line" ]; then
-        echo $line >> $2
+        echo $line >> $outputFile
     fi
 done < $tmp1
 
